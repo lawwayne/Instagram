de6d6fe09a958fc6b70ea4c1fcb0984a933c0e92
lawwayne <72894553+lawwayne@users.noreply.github.com>
2022-03-19 23:52:22 -0400

    Add files via upload

---
+++ Instagram/Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Student on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
